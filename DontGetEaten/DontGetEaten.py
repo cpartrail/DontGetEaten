aaf4e84a57e81e56c6acf0433cff13019c3aa83c
cpartrail <cpartrail@users.noreply.github.com>
2018-09-30 20:48:06 -0300

    Main Loop Update
    Greeting and continue prompt added in main loop.

--- DontGetEaten/DontGetEaten.py
+++ DontGetEaten/DontGetEaten.py
@@ -1,7 +1,19 @@
+from player import Player
+
 def main():
-    print("You're in a scary cell. Have a nap until it goes away.")
+    player_continue = '0'
+
+    player1 = Player()
+    player_name = input('Enter your name: ')
+    player1.namePlayer(player_name)
     
+    print("You're in a scary cell ", player1.name, ". Have a nap until it goes away.", sep='')
+
+    while player_continue == '0':
+        player_continue = input("Continue? ")
+        
 
 if __name__ == "__main__": main ()
+
 
     
