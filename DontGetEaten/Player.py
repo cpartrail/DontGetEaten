4cc48389af34593a53c6ce6f8c0df3e5ec5f56c3
cpartrail <43617952+cpartrail@users.noreply.github.com>
2018-09-30 17:10:37 -0300

    Uploading Player Class

---
+++ DontGetEaten/Player.py
@@ -0,0 +1,47 @@
+class Player:
+
+    def __init__(self):
+        self.name = 'nobody'
+        self.alive = True
+        self.inventory = []
+
+    def killPlayer(self):
+        self.alive = False
+
+    def revivePlayer(self):
+        self.alive = True
+
+    def namePlayer(self, name):
+        self.name = name
+
+    def addItem(self, item):
+        self.inventory.append(item)
+
+    def removeItem(self, item):
+        self.inventory.remove(item)
+
+    def showItems(self):
+        for items in self.inventory: print(items)
+
+    
+    
+
+'''
+=================
+    TESTING
+=================
+'''
+
+'''
+Player1 = Player()
+Player1.namePlayer('Corey')
+Player1.killPlayer()
+print("Player 1 name is", Player1.name)
+print("Player 1 is alive?", Player1.alive, "\n")
+      
+Player2 = Player()
+print("Player 2 name is", Player2.name)
+print("Player 2 is alive?", Player2.alive)
+'''
+
+        
