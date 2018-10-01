665f8d109c81a377437462341cf3446ed9efe132
cpartrail <cpartrail@users.noreply.github.com>
2018-10-01 14:41:09 -0300

    Intro Formatting Updated
    Updated intro prompts and formatting in Main

--- DontGetEaten/DontGetEaten.py
+++ DontGetEaten/DontGetEaten.py
@@ -1,14 +1,15 @@
 from player import Player
 
 def main():
-    player_continue = '0'
+    print("===================================\n         Don't Get Eaten\n===================================\n\n")
 
     player1 = Player()
     player_name = input('Enter your name: ')
     player1.namePlayer(player_name)
     
-    print("You're in a scary cell ", player1.name, ". Have a nap until it goes away.", sep='')
+    print("\n\nYou're in a scary cell ", player1.name, ". Have a nap until it goes away.", sep='')
 
+    player_continue = '0'
     while player_continue == '0':
         player_continue = input("Continue? ")
         
